d1288b0ca1d230bcbe506f85f1b82c47396ac3f8
likunlun <sz.lkl@163.com>
2021-10-27 22:08:06 +0800

    获取模块源文件路径

--- buildSrc/src/main/groovy/com/lkl/buildsrc/plugin/BuildSrcGroovyPlugin.groovy
+++ buildSrc/src/main/groovy/com/lkl/buildsrc/plugin/BuildSrcGroovyPlugin.groovy
@@ -1,12 +1,31 @@
 package com.lkl.buildsrc.plugin
 
+import com.android.build.gradle.internal.api.DefaultAndroidSourceSet
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.internal.file.DefaultSourceDirectorySet
+import org.gradle.api.plugins.JavaPluginConvention
 
 class BuildSrcGroovyPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
         println("BuildSrcGroovyPlugin ${project.path}")
+
+//        // 获取java模块源文件
+//        JavaPluginConvention javaPluginConvention = project.getConvention().getPlugin(JavaPluginConvention.class)
+//        javaPluginConvention.getSourceSets().getByName("main").java {
+//            DefaultSourceDirectorySet directorySet ->
+//                directorySet.srcDirs.forEach {
+//                    println("src dir ${it.canonicalPath}")
+//                }
+//        }
+//
+//        // 获取Android模块源文件
+//        project.extensions.getByName("android").sourceSets.forEach { DefaultAndroidSourceSet sourceSet ->
+//            sourceSet.java.srcDirs.forEach {
+//                println("src dir ${it.canonicalPath}")
+//            }
+//        }
     }
 }
